4460c7d95c70924208421a1b405252ede5c59bd8
Ulysses Schultian <schultuj@mail.uc.edu>
2025-09-22 21:25:59 -0400

    Init

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numberOfSiblings = 2;
+        boolean isRaining = false;
+        double gasPricePerGallon = 3.89;
+        int favoriteNumber = 7;
+        double shoeSize = 10.5;
+        String birthMonth = "April";
+        String fullName = "Ulysses Schultian";
+
+
+        System.out.println("Number of siblings: " + numberOfSiblings);
+        System.out.println("Is it raining? " + isRaining);
+        System.out.println("Gas price per gallon: $" + gasPricePerGallon);
+        System.out.println("My favorite number is: " + favoriteNumber);
+        System.out.println("My shoe size is: " + shoeSize);
+        System.out.println("I was born in: " + birthMonth);
+        System.out.println("My full name is: " + fullName);
+    }
+}
